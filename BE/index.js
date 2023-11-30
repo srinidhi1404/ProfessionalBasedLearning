47bf23fe4047fde25cc9776967425003ecc0abe2
Srinidhi Balmuri <balmurisrinidhi@gmail.com>
2023-11-29 23:02:16 -0600

    Update index.js

--- BE/index.js
+++ BE/index.js
@@ -11,7 +11,12 @@ const fileupload = require("express-fileupload");
 const port = 3000;
 // security
 app.use(helmet());
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://professional-based-learning-qene.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 // cors
 app.use(cors());
 app.use(express.json());
