c80326acfc68da9530c1fd1ddfd75820e3166a9c
Srinidhi Balmuri <balmurisrinidhi@gmail.com>
2023-12-01 06:24:40 -0600

    final

--- BE/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  JWT_TOKEN_KEY: "dgp1",
-  CLOUDINARY_CLOUD_NAME: "dd1uzjqg8",
-  CLOUDINARY_API_KEY: 313911865384253,
-  CLOUDINARY_API_SECRET: "kW5s4_j6xxc_qoF6u1tbUVxcd3c",
-};
